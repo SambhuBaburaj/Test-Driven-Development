5b4fde25646cbf5204da84a55bfe95c6c359be9b
Sambhu Baburaj <sambhubaburah513@gmail.com>
2024-05-09 21:46:47 +0530

    test cases added

--- additionCalculator.test.js
+++ additionCalculator.test.js
@@ -2,4 +2,11 @@
 import additionCalculator from "./additionCalculator";
 test("addition of 4 and 6 to equal 10", () => {
   expect(additionCalculator(4, 6)).toBe(10);
+
+
+});
+test("addition of 8 and 6 to equal 14", () => {
+  expect(additionCalculator(8, 6)).toBe(14);
+
+
 });
